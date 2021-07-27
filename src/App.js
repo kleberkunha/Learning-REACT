91995b1af1d33c06a3297488b9e9a085803264b6
kleberkunha <kleberkunha@hotmail.com>
2021-07-27 20:09:09 +0200

    first commit of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid black;
+`;
+
+
 
 function App() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] =useState('');
+
+  const handleEmailInput = (e) =>{
+    setEmail( e.target.value);
+  }
+
+  const handlePasswordInput = (e) => {
+    setPassword (e.target.value);
+  }
+
+  const handleButton = () => {
+    alert(email+ ' '+ password);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <input placeholder="digite um email" type="email" value={email} onChange={handleEmailInput}/>
+  <input placeholder="digite uma senha" type="password" value={password} onChange={handlePasswordInput}/>
+  <button onClick={handleButton} >Dizer</button>
+    </>
   );
 }
-
 export default App;
