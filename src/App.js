d1e2b5de402a00aeb9ee57074c1432741440b1e5
kleberkunha <kleberkunha@hotmail.com>
2021-07-28 08:57:26 +0200

    Tip calculador at App.js

--- src/App.js
+++ src/App.js
@@ -2,49 +2,35 @@ import React, {useState} from 'react';
 import './App.css';
 import styled from 'styled-components';
 
-const Input = styled.input`
-  width: 400px;
-  height: 30px;
-  font-size: 16px;
-  padding: 10px;
-  border: 1px solid black;
-`;
 
+function App (){
 
-
-function App() {
-
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] =useState('');
-
-  const handleEmailInput = (e) =>{
-    setEmail( e.target.value);
+  const [total, setTotal] = useState('');
+  const handleTotal = (e) => {
+    setTotal (e.target.value);
   }
 
-  const handlePasswordInput = (e) => {
-    setPassword (e.target.value);
-  }
+  const [gorjeta, setGorjeta] = useState('');
 
-  const handleButton = () => {
-    alert(email+ ' '+ password);
+  const handleGorjeta = (e) => {
+    setGorjeta(e.target.value)
   }
-
-  const [ isLogged, setIsLogged ] = useState(false);
-  
+  let totalGorjeta = (total * gorjeta / 100);
+  let Total = parseInt(total);
+  let fullTotal = totalGorjeta + Total;
   return (
     <>
-      <input placeholder="digite um email" type="email" value={email} onChange={handleEmailInput}/>
-      <input placeholder="digite uma senha" type="password" value={password} onChange={handlePasswordInput}/>
-      <button onClick={handleButton} >Dizer</button>
-
-
-
-      {email.length > 0 && <p>{email.length} caractere{email.length != 1 ? 's' : ''}</p>}
-
-      {isLogged ? <button>Sair</button> : <button>Fazer login</button>}
+    <h1>Calculadora de Gorjeta</h1>
+    <p>Quanto deu a conta?</p>
+    <input type="number" value={total} onChange={handleTotal}></input>
+    <p>Qual a porcentagem da gorjeta ?</p>  
+    <input type="number" value={gorjeta} onChange={handleGorjeta}></input> 
+    <hr></hr>
+    <p>Subtotal: R$ {total}</p>
+    <p>Gorjeta({gorjeta}%): R$ {totalGorjeta}</p>
+    <h2>Total: R$ {fullTotal}</h2>
     </>
-    //Essa condicional esta dizendo..., Se o email.lenght for maior que zero, ele printa o texto caractere   &&  se o email.lengh no S for diferente de 1, ele coloca o S.
-
   );
 }
+
 export default App;
