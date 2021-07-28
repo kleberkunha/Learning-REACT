8b699ba1bd0707c599e1e7fa0f1e9690e3216616
kleberkunha <kleberkunha@hotmail.com>
2021-07-28 15:22:41 +0200

    Storagin string in localStorage App.js only

--- src/App.js
+++ src/App.js
@@ -4,56 +4,16 @@ import SearchBox from './components/SearchBox';
 
 function App (){
 
-  ///DISPLAYING LIST
-  const [searchText, setSeachText] = useState('');
+  const [name, setName] = useState (localStorage.getItem('name'));
 
-  //creating state
-  const [list, setList] = useState([]);
+  useEffect(() => {
+    localStorage.setItem('name', name);
 
-  useEffect(()=>{
-    setList([
-      {title:'Comprar o bolo', done:false},
-      {title:'Pegar o cachorro no Petshop', done:true},
-      {title:'Gravar aula', done:false}
-    ]);
-  }, []);
-
-
-
-  function addAction(newItem){
-    let newList = [...list, {title:newItem, done:false}];
-
-    setList(newList);
-  }
-
-
-  ///esta funçao esta reproduzindo o click, marcando ou desmarcando ,true or false;
-  function handleToggleDone(index){
-    let newList = [...list];
-    newList[index].done = !newList[index].done;
-
-    setList(newList);
-  }
-
-
+  },[name]);
 
   return (
     <>
-      <h1>Lista de tarefas</h1>
-
-      <SearchBox 
-        frasePadrao= "Adicione um item..."
-        onEnter={addAction}
-      />
-
-      <hr/>
-      <ul>
-        {list.map((item, index) => {
-          return(
-            <li key={index} onClick={() => {handleToggleDone(index)}}>{item.title} -- {item.done.toString()}</li>
-          );
-        })}
-      </ul>
+  <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
     </>
   );
 }
