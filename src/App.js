869ecfae4a0973af40439ccdfdee5795cef9f1e5
kleberkunha <kleberkunha@hotmail.com>
2021-07-28 08:06:12 +0200

    How to show output

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid black;
+`;
+
+
 
 function App() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] =useState('');
+
+  const handleEmailInput = (e) =>{
+    setEmail( e.target.value);
+  }
+
+  const handlePasswordInput = (e) => {
+    setPassword (e.target.value);
+  }
+
+  const handleButton = () => {
+    alert(email+ ' '+ password);
+  }
+
+  const [ isLogged, setIsLogged ] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input placeholder="digite um email" type="email" value={email} onChange={handleEmailInput}/>
+      <input placeholder="digite uma senha" type="password" value={password} onChange={handlePasswordInput}/>
+      <button onClick={handleButton} >Dizer</button>
+
+
+
+      {email.length > 0 && <p>{email.length} caractere{email.length != 1 ? 's' : ''}</p>}
+
+      {isLogged ? <button>Sair</button> : <button>Fazer login</button>}
+    </>
+    //Essa condicional esta dizendo..., Se o email.lenght for maior que zero, ele printa o texto caractere   &&  se o email.lengh no S for diferente de 1, ele coloca o S.
+
   );
 }
-
 export default App;
